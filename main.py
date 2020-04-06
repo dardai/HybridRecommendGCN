4093c89377e52a1bb072826d304e4a71c0ad9f86
yang100123 <823448049@qq.com>
2020-04-06 18:56:03 +0800

    2020.4.6 杨文勇 使用main运行

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
 
 #from bigraph.courseRecommend import Main
-
-from bigraph.coursePredeal import updateCourseDr
-
+from roc import courseRecommend2 as cR2
+from roc import draw2
+from bigraph import cR3
+import pandas as pd
 if __name__ == '__main__':
     print('test')
-    updateCourseDr()
+    #updateCourseDr()
+    #cR.Main()
+    #cR2.Main()
+    draw2.Main()
+
+
+
+    # pd.set_option('display.max_rows', 560)
+    #
+    # pd.set_option('display.max_columns', 560)
+    #
+    # pd.set_option('display.width', 500)
+    # cR3.Main()
 
 
 
