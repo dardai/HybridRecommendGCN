03e761c67629584b39561068b28f36731c3bed38
yang100123 <823448049@qq.com>
2020-11-20 15:34:30 +0800

    杨文勇 2020.11.20 可设定所需的秒数来取现在到所设秒数之前的增量数据

--- online/onlineRecommend.py
+++ online/onlineRecommend.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 import datetime
+import time
 
 from changedPredeal import updateCourseDrChanged
 from courseDifferStats import differFusion
 from changedBigraph import bigraphChangedMain
 from resultFusion import fusion
 
-d = datetime.datetime(2020, 8, 1, 0, 0, 0)
+#设定秒数
+changedTime =30
+d = time.time()
+d = d-changedTime
+d = datetime.datetime.fromtimestamp(d)
+
 updateCourseDrChanged(d)
 differFusion(d)
 bigraphChangedMain()
