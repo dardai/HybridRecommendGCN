4fe5b4d232aff68fc2cf5a4d60553e1b914c5d2b
dardai <daijienk@qq.com>
2020-12-06 23:27:58 +0800

    2020-12-6 歹杰 完成召回方法，目前可以拿到经过召回处理的学习记录

--- recall/course_type_based_recall.py
+++ recall/course_type_based_recall.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-
-import csv
-import numpy as np
+import datetime
 
 from utils.databaseIo import DatabaseIo
 from globalConst import DataBaseOperateType, getEnumValue
@@ -9,73 +7,81 @@ from globalConst import DataBaseOperateType, getEnumValue
 
 # 基于阈值，拿到一段时间内交互记录里的课程类别
 def get_types():
-    date_threshold = 1 #周期的阈值，暂定为1个月
-    original_course_ids = get_original_course_id(date_threshold)  # 拿下周期内的原始课程id
+    # 周期的阈值，暂定为1个月
+    date_threshold = 1
+    # 取出周期内的原始课程id
+    original_course_ids = get_original_course_id(date_threshold)
+    # 基于课程id找到课程类别id，利用type_id过滤course_id
+    target_course_ids = []
+    for course_id in original_course_ids:
+        course_ids = get_target_course_id(str(course_id[0]))
+        target_course_ids += course_ids
+
+    return target_course_ids
 
 
-    user_dict, course_dict, user_data_dict, \
-        course_data_dict = getAllUserAndCourse()
-
-    user_length = len(user_dict)
-    course_length = len(course_dict)
-    max_point = 1
-
-    for key, value in user_data_dict.items():
-        if max_point < value[1]:
-            max_point = value[1]
-    for index in range(user_length):
-        one_list = []
-        one_list.append(float(user_data_dict[index][1]) / max_point)
-        user_feature.append(one_list)
-    for index in range(course_length):
-        value = CourseType.CourseType_None
-        if index in course_data_dict.keys():
-            value = transformCourseType(course_data_dict[index][1])
-
-        course_feature.append(getEnumValue(value))
-
-    course_features = np.zeros((course_length, 136), dtype=np.float32)
-    for index in course_dict.keys():
-        other_index = course_feature[index] - 1
-        course_features[index][other_index] = 1
-
-    #print(user_feature)
-    #print('----------------')
-    #print(course_features)
-
-    return user_feature, course_features
-
-
+    # 基于阈值设定时间取回课程id
 def get_original_course_id(threshold):
-    sql = 'SELECT course_id FROM account_course'
+    m = datetime.datetime.now().month
+    if m == 1:
+        m = 12
+    else:
+        m -= threshold
+    if m < 10:
+        m = '0' + str(m)
+    else:
+        m = str(m)
+    time_line = str(datetime.datetime.now().year) + '-' + str(m)
+
+    sql = 'SELECT DISTINCT course_id FROM account_course5000 WHERE date_format(update_time,' \
+          ' \'%Y-%m-%d %H:%i:%s\') LIKE \'' + time_line + '____________\''
 
     dbHandle = DatabaseIo()
 
-    dataList = dbHandle.doSql(DataBaseOperateType.SearchMany, sql)
+    course_ids = dbHandle.doSql(DataBaseOperateType.SearchMany, sql)
 
-    user_data_dict = makeDataDict(user_dict, dataList)
-
-    return user_data_dict
+    return course_ids
 
 
-def getAllCourseInfo(course_dict):
-    sql = 'SELECT id, course_differ, course_type FROM course_info'
+# 基于给定课程id找到全部同类型课程id
+def get_target_course_id(course_id):
+    sql = 'SELECT DISTINCT id from course_classify5000 WHERE classify_id in (SELECT classify_id FROM course_classify5000 WHERE id=' + course_id + ')'
 
     dbHandle = DatabaseIo()
 
-    dataList = dbHandle.doSql(DataBaseOperateType.SearchMany, sql)
+    course_ids = dbHandle.doSql(DataBaseOperateType.SearchMany, sql)
 
-    course_data_dict = makeDataDict(course_dict, dataList)
-
-    return course_data_dict
+    return course_ids
 
 
-def makeDataDict(index_dict, data_list):
-    data_dict = {}
-    for index in index_dict.keys():
-        for one_data in data_list:
-            if index_dict[index] == one_data[0]:
-                data_dict[index] = one_data
-                break
+# 基于目标课程id对学习记录进行召回
+def get_recalled_records(target_course_ids):
+    # 把目标课程id拼成set，方便后续查找
+    course_id_set = "("
+    for id in target_course_ids:
+        course_id_set += '\''+str(id[0])+"\',"
+    course_id_set = course_id_set[0:-1]
+    course_id_set += ')'
+    records = get_target_records(course_id_set)
+    return records
 
-    return data_dict
+# 基于目标课程id对学习记录进行召回查询
+def get_target_records(course_id_set):
+    sql = 'SELECT * from account_course5000 WHERE course_id in ' + course_id_set
+
+    dbHandle = DatabaseIo()
+
+    records = dbHandle.doSql(DataBaseOperateType.SearchMany, sql)
+
+    return records
+
+
+# 召回的主流程函数
+def recall():
+    course_ids = get_types()
+    records = get_recalled_records(course_ids)
+    return records
+
+
+if __name__ == '__main__':
+    print(recall())
